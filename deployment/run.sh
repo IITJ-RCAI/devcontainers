39f7f7e948caa1727b55db400ab1df1b216cf596
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2021-07-18 04:42:27 +0530

    Add deployment scripts.

---
+++ deployment/run.sh
@@ -0,0 +1,34 @@
+# Get server IP and username
+read -p "Enter DGX server ip: " DGX_IP
+read -p "Enter DGX server username: " DGX_USER
+
+# Get tunnel port
+read -p "Enter local SSH tunnel port[8089]: " T_PORT
+if [ -z $T_PORT ]
+then
+    T_PORT=8089
+fi
+
+# Asy if new devcontainer creation is required
+read -p "Would you like to create a new devcontainer?[Y/n]: " IS_NEW_CONTAINER
+if [ -z $IS_NEW_CONTAINER ] || [ $IS_NEW_CONTAINER = 'y' ] || [ $IS_NEW_CONTAINER = 'Y']
+then
+    ssh $DGX_USER@$DGX_IP -t 'source <(curl -s )'
+fi
+
+echo "Building ssh tunnel..."
+echo "
+This terminal will now serves as a tunnel proxy to the DGX server.
+Please login to ssh once again at the prompt.
+Do not close this terminal untill you are working. If you end up closing this terminal, you can re-run this script and specify 'n' at the create new devcontainer prompt.
+
+To connect to the devcontainer you must configure your browser to use a SOCKS5 proxy via this URL,
+socks5://localhost:$T_PORT
+"
+
+# Build ssh tunnel and run command
+ssh -NTD $T_PORT $DGX_USER@$DGX_IP
+
+echo "SSH tunnel closed."
+echo "NOTE: If you created a devcontainer, it is still running...! Remember to stop it when you are done."
+read -p "Press enter to continue..."
---
+++ deployment/run_devcontainer.sh
@@ -0,0 +1,91 @@
+read -p "Enter devcontainer name: " name
+if [ -z "$name" ]
+then 
+    echo 'Name cannot be blank please try again!' 
+    exit 0 
+fi
+name=${name,,}
+
+read -p "Enter number of GPUs (0/1/2)[0]: " gpus
+gpus=${gpus:-0}
+if ! [[ "$gpus" =~ ^[0-2]$ ]]
+then 
+    echo 'Only one of 0/1/2 allowed.' 
+    exit 0 
+fi 
+
+mkdir -p $HOME/.devcontainer/${name}
+
+cat <<EOF >$HOME/.devcontainer/${name}/kubectl.yaml
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: devcontainer-$name
+spec:
+  template:
+    spec:
+      containers:
+      - name: $(whoami)-$name
+        image: mltooling/ml-workspace-gpu:latest
+        # imagePullPolicy: Always
+        env:
+          - name: SHUTDOWN_INACTIVE_KERNELS
+            value: "true"
+          - name: AUTHENTICATE_VIA_JUPYTER
+            value: "<generated>"
+          - name: WORKSPACE_SSL_ENABLED
+            value: "true"
+        resources:
+          limits:
+            nvidia.com/gpu: "$gpus"
+        volumeMounts:
+        - name: data
+          mountPath: /workspace/data
+        - name: nfs
+          mountPath: /workspace/storage
+        - name: home
+          mountPath: /home
+      volumes:
+      - name: data
+        hostPath:
+          path: /raid/$(whoami)
+          type: Directory
+      - name: nfs
+        hostPath:
+          path: /DATA1/$(whoami)
+          type: Directory
+      - name: home
+        hostPath:
+          path: /home/$(whoami)
+          type: Directory
+      restartPolicy: Never
+  backoffLimit: 1
+EOF
+
+cd $HOME/.devcontainer/${name}
+kubectl apply -f kubectl.yaml
+
+# Get pod data
+POD_NAME=$(kubectl get pods --selector=job-name=devcontainer-$name --output=jsonpath='{.items[-1].metadata.name}')
+
+POD_PHASE=$(kubectl get pods --selector=job-name=devcontainer-$name --output=jsonpath='{.items[-1].status.phase}')
+until [[ $POD_PHASE == Running ]]
+do
+  echo "Waiting for pod to start..."
+  sleep 5
+  POD_PHASE=$(kubectl get pods --selector=job-name=devcontainer-$name --output=jsonpath='{.items[-1].status.phase}')
+done
+
+POD_IP=$(kubectl get pods --selector=job-name=devcontainer-$name --output=jsonpath='{.items[-1].status.podIP}')
+
+TOKEN=$(kubectl logs $POD_NAME | sed -nE 's/http:\/\/localhost:80.0\/\?token=//p')
+while [ -z $TOKEN ]
+do
+  echo "Waiting for pod to start..."
+  sleep 5
+  TOKEN=$(kubectl logs $POD_NAME | sed -nE 's/ *http:\/\/localhost:80.0\/\?token=//p')
+done
+TOKEN=${${TOKEN##+( )}%%+( )}
+
+echo "Devcontainer running in pod '$POD_NAME' at ip: $POD_IP"
+echo "Connect at: https://${POD_IP}:8080/?token=$TOKEN"
